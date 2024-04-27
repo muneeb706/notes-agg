b9372c48b5a9c0de20e90c8836b56e94375dc061
Muneeb Shahid <muneeb706@gmail.com>
2024-04-26 23:11:58 -0700

    chore(github actions): Update coverage configuration and database settings #10

--- notes_agg/settings.py
+++ notes_agg/settings.py
@@ -102,11 +102,11 @@ LOGOUT_REDIRECT_URL = "/accounts/login"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
+        "NAME": os.environ.get("DB_NAME", ""),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
